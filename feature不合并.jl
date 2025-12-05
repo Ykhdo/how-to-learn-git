86017fcb645e1040e4d05f76f70459b996384d1f
Ykhdo <yangkh1028@163.com>
2025-12-05 11:54:30 +0800

    add 不合并

---
+++ feature不合并.jl
@@ -0,0 +1,11 @@
+# 制作计算器
+add(2, 3)
+
+# 制作一个函数，该函数接受两个参数，并返回它们的和
+function add(x, y)
+    return x + y    # 返回x和y的和                              
+end
+# 调用该函数并打印结果
+result = add(5, 7)
+
+println("The sum of 5 and 7 is: ", result)
